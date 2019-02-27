3e31bb5a1761cf6570e9964512790bdb64c82ea5
Niels <deklerkn@xs4all.nl>
2019-02-27 09:52:41 +0000

    initial commit

---
+++ evens.py
@@ -0,0 +1,24 @@
+def even_number_of_evens(numbers): 
+    return False
+    
+assert even_number_of_evens([]) == False, "No numbers"
+    
+    
+#   assert even_number_of_evens([2]) == False, "One even number"
+#    return False
+#   assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+#    return True
+
+#   assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
+#    return True
+    
+#   assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three are even"
+#    return False
+#   assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four are even"
+#    return True
+    
+#   assert even_number_of_evens([1, 3, 9]) == False, "No even numbers" 
+#    return False
+
+
+print ("All tests passed!")
